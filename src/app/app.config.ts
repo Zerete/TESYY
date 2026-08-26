d5be100a5129f3104c3b969a1329fc266bab81b4
Zerete <Luissantacruzriveros@gmail.com>
2026-08-26 17:04:27 -0400

    Update de home.html

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,31 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { MsalService, MsalGuard, MsalBroadcastService, MSAL_INSTANCE, MSAL_GUARD_CONFIG } from '@azure/msal-angular';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MsalService, MsalGuard, MsalBroadcastService, MSAL_INSTANCE, MSAL_GUARD_CONFIG, MSAL_INTERCEPTOR_CONFIG, MsalInterceptor, MsalInterceptorConfiguration } from '@azure/msal-angular';
 import { InteractionType } from '@azure/msal-browser';
 import { routes } from './app.routes';
 import { MSALInstanceFactory } from './factories/msal-instance.factory';
 
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  // Mapeamos la URL del backend al scope de Entra ID
+  protectedResourceMap.set('http://localhost:8081/api/*', ['api://1e8ab7c6-22fe-4cd0-9a84-b3ae3cf10659/OT.Create']);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap
+  };
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()), // Habilitamos HTTP
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
     {
       provide: MSAL_INSTANCE,
       useFactory: MSALInstanceFactory,
@@ -20,6 +38,10 @@ export const appConfig: ApplicationConfig = {
           scopes: ['openid', 'profile'],
         },
       },
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory,
     },
     MsalService,
     MsalGuard,
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +11,23 @@ import { AuthService } from '../../services/auth.service';
 })
 export class HomeComponent {
   private authService = inject(AuthService);
+  private http = inject(HttpClient);
+
   account = this.authService.getAccount();
+  mensajeApi: string = '';
 
   logout(): void {
     this.authService.logout();
+  }
+
+  llamarApi(): void {
+    this.http.get('http://localhost:8081/api/ordenes', { responseType: 'text' })
+      .subscribe({
+        next: (respuesta) => this.mensajeApi = respuesta,
+        error: (err) => {
+          console.error('Error al llamar a la API', err);
+          this.mensajeApi = 'Error de autorización o conexión. Revisa la consola.';
+        }
+      });
   }
 }
